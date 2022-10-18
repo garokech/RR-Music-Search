e3e8fa3f1517b64aa247e22f0fb59591ad0f78c6
Garo Kechkarian <garokechkarian@Garos-iMac.local>
2022-10-18 13:23:25 -0700

    Homework 10/18

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import { useState, useRef } from 'react'
-import Gallery from './components/Gallery'
-import SearchBar from './components/Searchbar'
-import { DataContext } from './context/DataContext'
-import { SearchContext } from './context/SearchContext'
 
-function App(){
+import {useState, useRef } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Gallery from './components/Gallery'
+import Searchbar from './components/Searchbar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
+import { Fragment } from 'react/cjs/react.production.min'
+
+import { DataContext } from './context/DataContext'
+import {SearchContext } from './context/SearchContext'
+
+function App() {
+	// let [search, setSearch] = useState('')
 	let [message, setMessage] = useState('Search for Music!')
 	let [data, setData] = useState([])
 	let searchInput = useRef('')
 
 	const API_URL = 'https://itunes.apple.com/search?term='
+
+	// useEffect(() => {
+	// 	if(search) {
+			
+	// 	}
+	// }, [search])
 	
 	const handleSearch = (e, term) => {
 		e.preventDefault()
@@ -26,20 +39,33 @@ function App(){
 			}
 		}
 		fetchData()
+		// setSearch(term)
 	}
 
 	return (
-		<div>
-			<SearchContext.Provider value={{
-				term: searchInput,
-				handleSearch: handleSearch
-			}}>
-				<SearchBar />
-			</SearchContext.Provider>
-			{message}
-			<DataContext.Provider value={data}>
-				<Gallery />
-			</DataContext.Provider>
+		<div className="App">
+			<Router>
+				<Routes>
+					<Route path="/" element={
+						<Fragment>
+							<SearchContext.Provider value={{
+							term: searchInput,
+							handleSearch: handleSearch
+							}}>
+							<Searchbar />
+							</SearchContext.Provider>
+							{/* <SearchBar handleSearch = {handleSearch}/> */}
+							<DataContext.Provider value={data}>
+							<Gallery />
+							<AlbumView />
+							<ArtistView />
+							</DataContext.Provider>
+						</Fragment>
+						} />
+						<Route path="/album/:id" element={<AlbumView />} />
+						<Route path="/artist/:id" element={<ArtistView />} />
+				</Routes>
+			</Router>
 		</div>
   	);
 }
